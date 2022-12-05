eb16c2c7e0d482f666bad7fcba5f547af1b5fcc9
Katya <www@mail.ru>
2022-12-05 12:15:15 +0300

    DONQ

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Task 3
+Console.WriteLine("Insert number:");
+ int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 1)
+{
+    Console.WriteLine("Number" + n + " is an UNEVEN number");
+}
+else
+{
+    Console.WriteLine("Number" + n + "is an EVEN number");
+}
+
